061c8853e9e16b13387e4cfcf49641b8c276aae8
Marcos Velasco <devslife7@gmail.com>
2020-07-28 15:15:03 -0400

    fixed nested api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,11 @@ class UsersController < ApplicationController
 
   def index
     users = User.all
-    render json: users, include: [:courses]
-    # render :json => users.to_json(:include => { :courses => {:include =>:c} })
-    # render json: CompanySerializer.new(@companies, include: [:projects, :company_projects, :employees]).serialized_json
+
+    render json: users, :include => {
+      :courses => {
+        :include => {:cards => {:except => [:updated_at, :created_at]}},
+        :except => [:updated_at, :created_at]},
+    }, :except => [:updated_at]
   end
 end
