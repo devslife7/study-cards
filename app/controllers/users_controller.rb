ad9c24b7e15c70ff4a0421cac61b2ffc811a53c9
Marcos Velasco <devslife7@gmail.com>
2020-07-28 13:23:53 -0400

    fixing serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+
+  def index
+    users = User.all
+    render json: users, include: [:courses]
+    # render :json => users.to_json(:include => { :courses => {:include =>:c} })
+    # render json: CompanySerializer.new(@companies, include: [:projects, :company_projects, :employees]).serialized_json
+  end
 end
