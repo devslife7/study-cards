c5ef7bd0e53cbce88a2b8eee9a3e7ca9fbce7100
Marcos Velasco <devslife7@gmail.com>
2020-07-30 20:41:38 -0400

    add controller actions for new course feature

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,19 @@
 class CoursesController < ApplicationController
+
+  def index
+    courses = Course.all
+    render json: courses, include: [:cards]
+  end
+
+  def create
+    newUser = Course.create(courseParams)
+
+    render json: newUser, include: [:cards]
+  end
+
+  private
+
+  def courseParams
+    params.require(:course).permit(:name, :private, :user_id)
+  end
 end
